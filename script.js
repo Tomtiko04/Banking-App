98f38a90d042330578bc9213dafedc9577ab3945
Ogunneye <83877877+Tomtiko04@users.noreply.github.com>
2022-08-22 19:55:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const labelWelcome = document.querySelector('.welcome');
+const labelDate = document.querySelector('.date');
+const labelBalance = document.querySelector('.balance__value');
+const labelSumIn = document.querySelector('.summary__value--in');
+const labelSumOut = document.querySelector('.summary__value--out');
+const labelSumInterest = document.querySelector('.summary__value--interest');
+const labelTimer = document.querySelector('.timer');
