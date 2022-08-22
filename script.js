395815c984e211e50a17619571d4d2e0ed3603fb
Ogunneye <83877877+Tomtiko04@users.noreply.github.com>
2022-08-22 19:56:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,5 @@ const labelSumIn = document.querySelector('.summary__value--in');
 const labelSumOut = document.querySelector('.summary__value--out');
 const labelSumInterest = document.querySelector('.summary__value--interest');
 const labelTimer = document.querySelector('.timer');
+const containerApp = document.querySelector('.app');
+const containerMovements = document.querySelector('.movements');
